7d71121b03168440e3642b75fbde7b324ab31aae
Qw3rtyum <pablolylm@gmail.com>
2023-02-02 10:53:49 +0100

    Commit inicial

---
+++ System.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class System here.
+ * 
+ * @author Qw3rtyum 
+ * @version 0.1
+ */
+public class System
+{
+    private int numGears;
+    private boolean isRunning;
+
+    /**
+     * Constructor de la clase System
+     */
+    public System(int newGears, boolean state)
+    {
+        numGears = newGears;
+        isRunning = state;
+    }
+}
