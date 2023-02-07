7e23d8d3450962a4273680a1d3288a5b998affd6
Qw3rtyum <pablolylm@gmail.com>
2023-02-07 12:09:17 +0100

    Añadido contructor a clase Gear

--- Gear.java
+++ Gear.java
@@ -1,6 +1,13 @@
 public class Gear 
 {
-   private int numTeeth;
-   private double diameter;
-   private GearSystem gearSystem;
+    private int numTeeth;
+    private double diameter;
+    private GearSystem gearSystem;
+   
+    public Gear(int newTeeth, double newDiameter, int newGears, boolean state)
+    {
+        numTeeth = newTeeth;
+        diameter = newDiameter;
+        gearSystem = new GearSystem(newGears, state);
+    }
 }
